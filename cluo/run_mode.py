149e19b6fdfaa9b63b9026b86328b550b5873f2b
Chris Williams <50159166+cwlms@users.noreply.github.com>
2025-12-29 14:34:11 -0700

    Update run_mode.py

--- cluo/run_mode.py
+++ cluo/run_mode.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class RunMode(Enum):
-    """Modes in which pipelines can be run.  Controlls behavior related to sink and offset increment."""
+    """Modes in which pipelines can be run.  Controls behavior related to sink and offset increment."""
 
     WRITE = "WRITE"
     PREVIEW = "PREVIEW"
